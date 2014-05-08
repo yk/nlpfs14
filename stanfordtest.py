f9bd2254991382d3ac7e9a93c4eb9a18c0ebe344
yk <yk@yk-Octa.org>
2014-05-08 02:51:01 +0200

    pretty print standfordtest.py

--- stanfordtest.py
+++ stanfordtest.py
@@ -1,5 +1,5 @@
-import nlpio
+import nlpio,json
 
 if __name__ == '__main__':
-    print nlpio.stanfordParse('The world is so pretty.')
-    print nlpio.stanfordParse('I like trains.')
+    print json.dumps(nlpio.stanfordParse('The world is so pretty.'),indent=4)
+    print json.dumps(nlpio.stanfordParse('I like trains. They are nice and clean, almost as clean as their tracks.'),indent=4)
